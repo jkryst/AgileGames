b905a6791b77fb5933bc9eee00710da41d2294b1
barbara.kowalewska <bbkowalewska@gmail.com>
2023-03-02 10:53:57 +0100

    02-Second draft

--- agilegames/src/main/java/christmas/ChristmasTree.java
+++ agilegames/src/main/java/christmas/ChristmasTree.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 import static java.lang.Integer.parseInt;
 
 public class ChristmasTree {
-    public static void tree(int n) {
+    public static String[] tree(int n) {
+        if(n<=0){
+            return new String[0];
+        }
         int center = 2 * n;
+        String[] treeList = new String[n];
         StringBuilder line;
         for (int i = 0; i < n; i++) {
             line = new StringBuilder();
             for (int j = 0; j < center; j++)
                 line.append(" ");
             int j = 0;
-            for (; j < i ; j++) {
+            for (; j < i; j++) {
                 line.append("/");
             }
             line.append("A");
@@ -21,9 +25,15 @@ public class ChristmasTree {
             for (; j < 2 * i + 1; j++)
                 line.append("\\");
             center--;
-            System.out.println(line);
+            treeList[i] = String.valueOf(line);
         }
+        return treeList;
+    }
 
+    static void printList(String[] text){
+        for (String s: text) {
+            System.out.println((s));
+        }
     }
 
     public static void main(String[] args) {
@@ -32,7 +42,7 @@ public class ChristmasTree {
             Scanner in = new Scanner(System.in);
             String s = in.nextLine();
             int N = parseInt(s);
-            tree(N);
+            printList(tree(N));
         } catch (NumberFormatException ex) {
             System.out.println("To nie jest liczba! \nPodaj liczbę:");
         }
